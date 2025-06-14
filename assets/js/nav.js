abc4afcca60dd3e1d31befe26f2853d23295cd14
Enio Aiello <aielloenio@icloud.com>
2025-06-14 21:13:55 +0200

    Ajout d'un champ de recherche, mise à jour des icônes et ajustements des comportements des boutons pour améliorer l'UX.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Définition des éléments de la nav
     const menuButton = document.querySelector("#menu-button");
-    const downloadButton = document.querySelector("#install-button");
+    const downloadButton = document.querySelector("#download-button");
     const updateButton = document.querySelector("#update-button");
     const uninstallButton = document.querySelector("#uninstall-button");
     const settingsButton = document.querySelector("#settings-button");
+    const searchInput = document.querySelector("#search-input");
 
     // Définition des containers
     const downloadContainer = document.querySelector("#download");
     const updateContainer = document.querySelector("#update");
     const uninstallContainer = document.querySelector("#uninstall");
-    const welcomeContainer = document.querySelector("#welcome-alert");
     const settingsContainer = document.querySelector("#settings");
 
     // Définition des icônes
     const downloadIcon = "<i class=\"bi bi-arrow-down-circle\"></i>";
     const selectedDownloadIcon = "<i class=\"bi bi-arrow-down-circle-fill\"></i>";
-    const updateIcon = "<i class=\"bi bi-arrow-repeat\"></i>";
-    const selectedUpdateIcon = "<i class=\"bi bi-arrow-repeat-fill\"></i>";
+    const updateIcon = "<i class=\"bi bi-arrow-up-circle\"></i>";
+    const selectedUpdateIcon = "<i class=\"bi bi-arrow-up-circle-fill\"></i>";
     const uninstallIcon = "<i class=\"bi bi-trash\"></i>";
     const selectedUninstallIcon = "<i class=\"bi bi-trash-fill\"></i>";
     const settingsIcon = "<i class=\"bi bi-gear\"></i>";
@@ -31,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Fonction pour afficher la page d'installation d'applications
     function showInstall() {
-        welcomeContainer.classList.add("d-none");
         downloadContainer.classList.remove("d-none");
         updateContainer.classList.add("d-none");
         uninstallContainer.classList.add("d-none");
@@ -40,11 +39,11 @@ document.addEventListener('DOMContentLoaded', () => {
         updateButton.innerHTML = updateIcon;
         uninstallButton.innerHTML = uninstallIcon;
         settingsButton.innerHTML = settingsIcon;
+        searchInput.disabled = false;
     }
 
     // Fonction pour afficher la page de mise à jour d'applications
     function showUpdate() {
-        welcomeContainer.classList.add("d-none");
         downloadContainer.classList.add("d-none");
         updateContainer.classList.remove("d-none");
         uninstallContainer.classList.add("d-none");
@@ -53,11 +52,11 @@ document.addEventListener('DOMContentLoaded', () => {
         updateButton.innerHTML = selectedUpdateIcon;
         uninstallButton.innerHTML = uninstallIcon;
         settingsButton.innerHTML = settingsIcon;
+        searchInput.disabled = true;
     }
 
     // Fonction pour afficher la page de désinstallation d'applications
     function showUninstall() {
-        welcomeContainer.classList.add("d-none");
         downloadContainer.classList.add("d-none");
         updateContainer.classList.add("d-none");
         uninstallContainer.classList.remove("d-none");
@@ -66,16 +65,11 @@ document.addEventListener('DOMContentLoaded', () => {
         updateButton.innerHTML = updateIcon;
         uninstallButton.innerHTML = selectedUninstallIcon;
         settingsButton.innerHTML = settingsIcon;
+        searchInput.disabled = true;
     }
 
     // Fonction pour afficher les paramètres
     function showSettings() {
-        /*document.querySelector("#settings").classList.remove("d-none");
-        document.querySelector("#applications-container").classList.add("d-none");
-        document.querySelector("#application-detail").classList.add("d-none");
-        document.querySelector("#search-input").disabled = true;
-        document.querySelector("#welcome-alert").classList.add("d-none");*/
-        welcomeContainer.classList.add("d-none");
         downloadContainer.classList.add("d-none");
         updateContainer.classList.add("d-none");
         uninstallContainer.classList.add("d-none");
@@ -84,9 +78,16 @@ document.addEventListener('DOMContentLoaded', () => {
         updateButton.innerHTML = updateIcon;
         uninstallButton.innerHTML = uninstallIcon;
         settingsButton.innerHTML = selectedSettingsIcon;
+        searchInput.disabled = true;
     }
 
     // Écouteurs d'événements pour les boutons de la nav
     menuButton.addEventListener("click", showHome);
+    downloadButton.addEventListener("click", showInstall);
+    updateButton.addEventListener("click", showUpdate);
+    uninstallButton.addEventListener("click", showUninstall);
     settingsButton.addEventListener("click", showSettings);
+
+    // Écouteurs d'événements pour les boutons spéciaux sur la page
+    document.querySelector("#install-page-button").addEventListener("click", showInstall);
 });
